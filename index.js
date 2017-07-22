4339ec967a646403b8f70f2e651d58c12b1875b3
Stuart Langridge <sil@kryogenix.org>
2017-07-22 11:55:52 +0100

    Handle tere being no events (which happens at weekends a lot) with a message about it rather than just no entries at all

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ ical.fromURL(BIO, {}, (err, data) => {
     let out = [];
     console.log("Got the calendar ICS file");
     let allEvents = 0;
+    let closestFutureEventTime = 999999999999999999;
     for (let k in data) {
         if (data.hasOwnProperty(k)) {
             let ev = data[k];
-            if (!ev.start) continue;
+            if (!ev.start) {
+                if (process.env.DEBUG) { console.log("Skipping", ev.summary, "for no start date"); }
+                continue;
+            }
             allEvents += 1;
 
             /* get all events which are today or tomorrow */
@@ -25,7 +29,19 @@ ical.fromURL(BIO, {}, (err, data) => {
             let tomorrowUnix = tomorrow.unix();
             let endTomorrowUnix = endTomorrow.unix();
             let thisStart = ev.start.getTime() / 1000;
-            if (thisStart < todayUnix || thisStart > endTomorrowUnix) continue;
+
+            if (thisStart < closestFutureEventTime && thisStart > endTomorrowUnix) {
+                closestFutureEventTime = thisStart;
+            }
+
+            if (thisStart < todayUnix) {
+                if (process.env.DEBUG) { console.log("Skipping", ev.summary, "for being before today"); }
+                continue;
+            }
+            if (thisStart > endTomorrowUnix) {
+                if (process.env.DEBUG) { console.log("Skipping", ev.summary, "for being after tomorrow"); }
+                continue;
+            }
             let dayName = "today";
             if (thisStart > tomorrowUnix) { dayName = "tomorrow"; }
             let eventM = moment(ev.start);
@@ -61,7 +77,27 @@ ical.fromURL(BIO, {}, (err, data) => {
     /* write the events as JSON, and upload them to surge.sh */
     out.sort((a,b) => a.updateDate.localeCompare(b.updateDate));
     console.log("Finished processing " + allEvents + " events and found " + out.length + " upcoming.");
+
+    if (out.length == 0) {
+        console.log("No events today or tomorrow, so adding a pseudo event explaining when the next one is.");
+        let eventM = moment(closestFutureEventTime * 1000);
+        let timeDesc = eventM.format("h:mma") + " on " + eventM.format("dddd");
+        out.push({
+            titleText: "No events for a while",
+            mainText: "There are no events today or tomorrow. The next event is at " + timeDesc + ".",
+            uid: "noevents",
+            updateDate: eventM.format("YYYY-MM-DDTHH:mm:ss") + ".0Z",
+            redirectionUrl: "https://calendar.birmingham.io/",
+            fetchedAt: moment()
+        })
+    }
+
     let output = JSON.stringify(out, null, 2);
+    if (process.env.DEBUG) {
+        console.log(output);
+        process.exit();
+    }
+
     fs.writeFile(__dirname + "/site/bio.json", output, {encoding: "utf-8"}, err => {
         if (err) { console.error(err); process.exit(2); }
         console.log("Output JSON written.");
